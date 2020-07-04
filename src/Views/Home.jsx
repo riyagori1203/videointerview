82e2604d0cfa2444a6f69f0ece09982f8ee96af2
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2020-07-05 02:00:23 +0530

    Installed react-bootstrap

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Button, ButtonGroup, Container } from 'react-bootstrap'
 
 export default function Home() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <Container>
+                <h1>Home Page</h1>
+                <ButtonGroup>
+                {
+                    [
+                        'primary',
+                        'success',
+                        'warning',
+                        'info',
+                        'light',
+                        'secondary',
+                        'dark',
+                        'danger'
+                    ].map(variant => (
+                        <Button variant={variant}>Yes this works</Button>
+                    ))
+                }
+                </ButtonGroup>
+            </Container>
         </div>
     )
 }
